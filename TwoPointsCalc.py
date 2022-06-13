acad79ffb8d87acb54a264ee8b9c93e2c849d97c
FijianRocket <EightTheMate@protonmail.com>
2022-06-13 08:40:43 -0700

    Code cleanup and a small UI impromvent.

--- TwoPointsCalc.py
+++ TwoPointsCalc.py
@@ -1,22 +1,22 @@
 """ A simple calculator that find different information and applies formulas on two points """
 
 def twoPointsCalc(x1,y1,x2,y2):
-    m = (y2-y1)/(x2-x1)
-    b = (m*(-x2))+y2
-    r = (y2/y1)**(1/(x2-x1))
+    m = (y2 - y1) / (x2 - x1)
+    b = (m * (-x2)) + y2
+    r = (y2 / y1) ** (1 / (x2 - x1))
 
-    print("\n---------------------------------------------------------------------------\n")
+    print("\n-------------------------- Two Points Calculator --------------------------\n")
 
     print("Point-Slope Form: " + "y" + "-" + str(y2) + " = " + str(m) + "(x-" + str(x2) + ")")
-    print("Slope-Intercept Form: " + "y = " + str(m) + "x" + "+" + str(b))
-    print("Geometric Form: " + str(y1) + "(" + str(r) + ")^{\left(x-" + str(x1) + r"\right)}")
+    print("Slope-Intercept Form: " + "y = " + str(m) + "x" + " + " + str(b))
+    print("Geometric Form: " + str(y1) + "(" + str(r) + ") ^ {\left(x-" + str(x1) + r"\right)}")
 
     print("")
 
     print("Range: " + "{" + str(y1) + " < y < " + str(y2) + "}")
     print("Domain: " + "{" + str(min(x1,x2)) + " < x < " + str(max(x1,x2)) + "}")
 
-    print("\n---------------------------------------------------------------------------\n")
+    print("\n-------------------------------- Output -----------------------------------\n")
 
 twoPointsCalc(
     float(input("X1: ")),
